bd43e9835d14f61576471df571905297f3c12474
Philip Åkesson <philip.akesson@gmail.com>
2011-03-16 22:15:22 +0100

    Add GUI buttons (Back, Home, Menu). Cleanup code.

--- gtk-ui/gtk-ui.c
+++ gtk-ui/gtk-ui.c
@@ -7,7 +7,6 @@
 #include <pthread.h>
 #include <signal.h>
 
-
 #include <fcntl.h>
 #include <linux/input.h>
 #include <linux/fb.h>
@@ -21,6 +20,9 @@
 #define IMAGE_WIDTH  640
 #define IMAGE_HEIGHT 480
 
+#define EV_PRESSED  1
+#define EV_RELEASED 0
+
 static GdkPixmap *pixmap = NULL;
 guchar rgbbuf[IMAGE_WIDTH * IMAGE_HEIGHT * 3];
 static int currently_drawing = 0;
@@ -32,14 +34,13 @@ int                      stride; /* size of stride in pixel */
 struct fb_var_screeninfo vi;
 struct fb_fix_screeninfo fi;
 
-/* Touch events */
-static char TOUCH_DEVICE[PATH_MAX] = "/dev/input/event2";
-static int touchfd = -1;
+/* Input events */
+static char INPUT_DEVICE[PATH_MAX] = "/dev/input/event2";
+static int inputfd = -1;
 static int xmin, xmax;
 static int ymin, ymax;
 
-static gboolean
-configure_event(GtkWidget *widget, GdkEventConfigure *event)
+static gboolean configure_event(GtkWidget *widget, GdkEventConfigure *event)
 {
     return TRUE;
 }
@@ -52,7 +53,7 @@ void *do_draw(void *ptr)
     sigemptyset(&sigset);
     sigaddset(&sigset, SIGALRM);
 
-    while(1){
+    while (1) {
         while (sigwaitinfo(&sigset, &info) > 0) {
             currently_drawing = 1;
 
@@ -112,8 +113,7 @@ gboolean timer_exe(GtkWidget *widget)
     return TRUE;
 }
 
-static gboolean
-expose_event(GtkWidget *widget, GdkEventExpose *event)
+static gboolean expose_event(GtkWidget *widget, GdkEventExpose *event)
 {
     cairo_t *cr = gdk_cairo_create(widget->window);
     gdk_cairo_set_source_pixmap(cr, pixmap, 0, 0);
@@ -123,83 +123,103 @@ expose_event(GtkWidget *widget, GdkEventExpose *event)
     return FALSE;
 }
 
-static void init_touch()
+static void init_input_device()
 {
     struct input_absinfo info;
     
-    if((touchfd = open(TOUCH_DEVICE, O_RDWR)) == -1) {
-        printf("cannot open touch device %s\n", TOUCH_DEVICE);
+    if((inputfd = open(INPUT_DEVICE, O_RDWR)) == -1) {
+        printf("Cannot open input device %s\n", INPUT_DEVICE);
         exit(EXIT_FAILURE);
     }
     
     // Get the Range of X and Y
-    if(ioctl(touchfd, EVIOCGABS(ABS_X), &info)) {
-        printf("cannot get ABS_X info, %s\n", strerror(errno));
+    if(ioctl(inputfd, EVIOCGABS(ABS_X), &info)) {
+        printf("Cannot get ABS_X info, %s\n", strerror(errno));
         exit(EXIT_FAILURE);
     }
     
     xmin = info.minimum;
     xmax = info.maximum;
-    if (xmax)
-        printf("touchscreen xmin=%d xmax=%d\n", xmin, xmax);
-    else
-        printf("touchscreen has no xmax: using emulator mode\n");
+    if (xmax) {
+        printf("Touch device xmin=%d xmax=%d\n", xmin, xmax);
+    } else {
+        printf("Touch device has no xmax: using emulator mode\n");
+    }
 
-    if(ioctl(touchfd, EVIOCGABS(ABS_Y), &info)) {
-        printf("cannot get ABS_Y, %s\n", strerror(errno));
+    if(ioctl(inputfd, EVIOCGABS(ABS_Y), &info)) {
+        printf("Cannot get ABS_Y, %s\n", strerror(errno));
         exit(EXIT_FAILURE);
     }
     ymin = info.minimum;
     ymax = info.maximum;
     if (ymax)
-        printf("touchscreen ymin=%d ymax=%d\n", ymin, ymax);
+        printf("Touch device ymin=%d ymax=%d\n", ymin, ymax);
     else
-        printf("touchscreen has no ymax: using emulator mode\n");
+        printf("Touch device has no ymax: using emulator mode\n");
 }
 
-static void cleanup_touch()
+static void cleanup_input()
 {
-    if(touchfd != -1) {
-        close(touchfd);
+    if(inputfd != -1) {
+        close(inputfd);
     }
+}
+
+void injectKeyEvent(unsigned int code, unsigned int value)
+{
+    struct input_event ev;
+    
+    memset(&ev, 0, sizeof(ev));
+    gettimeofday(&ev.time, 0);
+    ev.type = EV_KEY;
+    ev.code = code;
+    ev.value = value;
+    if(write(inputfd, &ev, sizeof(ev)) < 0) {
+        printf("Event failed, %s\n", strerror(errno));
+    }
+
+    /*gettimeofday(&ev.time,0);
+    ev.type = EV_SYN;
+    ev.code = 0;
+    ev.value = 0;
+    if(write(inputfd, &ev, sizeof(ev)) < 0) {
+        printf("Event failed, %s\n", strerror(errno));
+    }*/
 }
 
 void injectTouchEvent(int down, int x, int y)
 {
     struct input_event ev;
 
-    // Re-calculate the final x and y if xmax/ymax are specified
+    /* Re-calculate the final x and y if xmax/ymax are specified */
     if (xmax) x = xmin + (x * (xmax - xmin)) / (vi.xres);
     if (ymax) y = ymin + (y * (ymax - ymin)) / (vi.yres);
     
     memset(&ev, 0, sizeof(ev));
 
-    // Then send a BTN_TOUCH
+    // Send a BTN_TOUCH
     gettimeofday(&ev.time,0);
     ev.type = EV_KEY;
     ev.code = BTN_TOUCH;
     ev.value = down;
-    if(write(touchfd, &ev, sizeof(ev)) < 0) {
-        printf("write event failed, %s\n", strerror(errno));
+    if(write(inputfd, &ev, sizeof(ev)) < 0) {
+        printf("Write event failed, %s\n", strerror(errno));
     }
 
-    // Then send the X
     gettimeofday(&ev.time,0);
     ev.type = EV_ABS;
     ev.code = ABS_X;
     ev.value = x;
-    if(write(touchfd, &ev, sizeof(ev)) < 0)
-    {
-        printf("write event failed, %s\n", strerror(errno));
+    if(write(inputfd, &ev, sizeof(ev)) < 0) {
+        printf("Write event failed, %s\n", strerror(errno));
     }
 
-    // Then send the Y
     gettimeofday(&ev.time,0);
     ev.type = EV_ABS;
     ev.code = ABS_Y;
     ev.value = y;
-    if(write(touchfd, &ev, sizeof(ev)) < 0) {
-        printf("write event failed, %s\n", strerror(errno));
+    if(write(inputfd, &ev, sizeof(ev)) < 0) {
+        printf("Write event failed, %s\n", strerror(errno));
     }
 
     // Finally send the SYN
@@ -207,15 +227,14 @@ void injectTouchEvent(int down, int x, int y)
     ev.type = EV_SYN;
     ev.code = 0;
     ev.value = 0;
-    if(write(touchfd, &ev, sizeof(ev)) < 0) {
-        printf("write event failed, %s\n", strerror(errno));
+    if(write(inputfd, &ev, sizeof(ev)) < 0) {
+        printf("Write event failed, %s\n", strerror(errno));
     }
 
-    printf("injectTouchEvent (x=%d, y=%d, down=%d)\n", x , y, down);
+    /*printf("injectTouchEvent (x=%d, y=%d, down=%d)\n", x, y, down);*/
 }
 
-static gboolean button_press_event(GtkWidget      *widget,
-                                   GdkEventButton *event )
+static gboolean button_press_event(GtkWidget *widget, GdkEventButton *event)
 {
     if (event->button == 1) {
         injectTouchEvent(1, event->x, event->y);
@@ -224,19 +243,16 @@ static gboolean button_press_event(GtkWidget      *widget,
     return TRUE;
 }
 
-static gboolean button_release_event(GtkWidget      *widget,
-                                     GdkEventButton *event )
+static gboolean button_release_event(GtkWidget *widget, GdkEventButton *event, gpointer user_data)
 {
     if (event->button == 1) {
-        //injectTouchEvent(0, event->x, event->y);
+        injectTouchEvent(0, event->x, event->y);
     }
-    injectTouchEvent(0, event->x, event->y);
 
     return TRUE;
 }
 
-static gboolean motion_notify_event(GtkWidget *widget,
-                                    GdkEventMotion *event)
+static gboolean motion_notify_event(GtkWidget *widget, GdkEventMotion *event, gpointer user_data)
 {
     int x, y;
     GdkModifierType state;
@@ -260,8 +276,29 @@ static gboolean motion_notify_event(GtkWidget *widget,
 
 static void destroy(GtkWidget *widget, gpointer data)
 {
-    cleanup_touch();
+    cleanup_input();
     gtk_main_quit();
+}
+
+static void back_button_clicked()
+{
+    printf("Back button pressed\n");
+    injectKeyEvent(KEY_BACKSPACE, EV_PRESSED);
+    injectKeyEvent(KEY_BACKSPACE, EV_RELEASED);
+}
+
+static void home_button_clicked()
+{
+    printf("Home button pressed\n");
+    injectKeyEvent(KEY_HOME, EV_PRESSED);
+    injectKeyEvent(KEY_HOME, EV_RELEASED);
+}
+
+static void menu_button_clicked()
+{
+    printf("Menu button pressed\n");
+    injectKeyEvent(KEY_LEFTMETA, EV_PRESSED);
+    injectKeyEvent(KEY_LEFTMETA, EV_RELEASED); //0x52
 }
 
 int main(int argc, char *argv[])
@@ -270,9 +307,11 @@ int main(int argc, char *argv[])
 
     GtkWidget *window;
     GtkWidget *drawing_area;
-    //GtkWidget *vbox;
+    GtkWidget *vbox;
+    GtkWidget *hbox;
+    GtkWidget *button;
     
-    //Block SIGALRM in the main thread
+    /* Block SIGALRM in the main thread */
     sigset_t sigset;
     sigemptyset(&sigset);
     sigaddset(&sigset, SIGALRM);
@@ -287,17 +326,17 @@ int main(int argc, char *argv[])
     
     gtk_init(&argc, &argv);
 
-    /* Deal with framebuffer */
+    /* Framebuffer */
     
     /* Open framebuffer */
     if (0 > (fd = open("/dev/fb0", O_RDWR))) {
-        printf("Fail to open fb\n");
+        printf("Failed to open fb\n");
         return -1;
     }
 
     /* Get fixed information */
     if(0 > ioctl(fd, FBIOGET_FSCREENINFO, &fi)) {
-        printf("Fail to get fixed info\n");
+        printf("Failed to get fixed info\n");
         return -1;
     }
 
@@ -319,47 +358,66 @@ int main(int argc, char *argv[])
     /* Calculate useful information */
     bpp = vi.bits_per_pixel >> 3;
     stride = fi.line_length / bpp;
-    
+
     /* Do GTK stuff */
     window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
     gtk_window_set_title((GtkWindow*)window, "ParallelDroid");
     g_signal_connect(window, "destroy", G_CALLBACK(destroy), NULL);
-    
-    //vbox = gtk_vbox_new(FALSE, 0);
-    //gtk_container_add(GTK_CONTAINER(window), vbox);
-    //gtk_widget_show(vbox);
-    
+
+    vbox = gtk_vbox_new(FALSE, 0);
+    gtk_container_add(GTK_CONTAINER(window), vbox);
+    gtk_widget_show(vbox);
+
     drawing_area = gtk_drawing_area_new();
     gtk_widget_set_size_request(GTK_WIDGET(drawing_area), vi.xres, vi.yres);
-    //gtk_box_pack_start(GTK_BOX(vbox), drawing_area, TRUE, TRUE, 0);
-    gtk_container_add(GTK_CONTAINER(window), drawing_area);
-
+    gtk_box_pack_start(GTK_BOX(vbox), drawing_area, TRUE, TRUE, 0);
+    gtk_widget_show(drawing_area);
     
-    init_touch();
+    init_input_device();
 
     /* Events */
     gtk_widget_set_events(drawing_area, GDK_EXPOSURE_MASK
                           | GDK_LEAVE_NOTIFY_MASK
                           | GDK_BUTTON_PRESS_MASK
+                          | GDK_BUTTON_RELEASE_MASK
                           | GDK_POINTER_MOTION_MASK
                           | GDK_POINTER_MOTION_HINT_MASK);
 
-    g_signal_connect(drawing_area, "motion_notify_event",
+    g_signal_connect(drawing_area, "motion-notify-event",
                      G_CALLBACK (motion_notify_event), NULL);
-    g_signal_connect(drawing_area, "button_press_event",
+    g_signal_connect(drawing_area, "button-press-event",
                      G_CALLBACK(button_press_event), NULL);
-    g_signal_connect(drawing_area, "button_release_event",
+    g_signal_connect(drawing_area, "button-release-event",
                      G_CALLBACK(button_release_event), NULL);
-    g_signal_connect(drawing_area, "expose_event",
+    g_signal_connect(drawing_area, "expose-event",
                      G_CALLBACK(expose_event), NULL);
-    g_signal_connect(drawing_area, "configure_event",
+    g_signal_connect(drawing_area, "configure-event",
                      G_CALLBACK(configure_event), NULL);
+                     
+    /* Create and add buttons */
+    hbox = gtk_hbox_new(FALSE, 0);
+    gtk_box_pack_start(GTK_BOX(vbox), hbox, FALSE, TRUE, 0);
+    gtk_widget_show(hbox);
+                     
+    button = gtk_button_new_with_label("Back");
+    gtk_box_pack_start(GTK_BOX(hbox), button, TRUE, TRUE, 0);
+    g_signal_connect(button, "clicked",
+                     G_CALLBACK(back_button_clicked), NULL);
+    gtk_widget_show(button);
+    
+    button = gtk_button_new_with_label("Home");
+    gtk_box_pack_start(GTK_BOX(hbox), button, TRUE, TRUE, 0);
+    g_signal_connect_swapped(button, "clicked",
+                             G_CALLBACK(home_button_clicked), NULL);
+    gtk_widget_show(button);
+    
+    button = gtk_button_new_with_label("Menu");
+    gtk_box_pack_start(GTK_BOX(hbox), button, TRUE, TRUE, 0);
+    g_signal_connect_swapped(button, "clicked",
+                             G_CALLBACK(menu_button_clicked), NULL);
+    gtk_widget_show(button);
 
-    gtk_widget_show(drawing_area);
     gtk_widget_show_all(window);
-
-    //gtk_widget_set_app_paintable(window, TRUE);
-    //gtk_widget_set_double_buffered(window, FALSE);
 
     pixmap = gdk_pixmap_new(drawing_area->window, IMAGE_WIDTH, 
                             IMAGE_HEIGHT, -1);
